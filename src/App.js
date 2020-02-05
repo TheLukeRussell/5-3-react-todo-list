f4648becc96b15f1577df0248f8824dee933f7ed
Luke Russell <thelukerussel21@gmail.com>
2020-02-05 12:46:15 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// class TodoForm extends Component{
+  // render() {
+  //   return(
+
+  //   )
+  // }
+// }
+
+// let Todolist = (props) => {
+
+
+
+  // return (
+  //   <ul>{todos}</ul>
+  // )
+
+// }
+
 class TodoApp extends Component {
+
+  state = ({
+    todos: [
+
+      {id: 0, text: 'Clean my room', isComplete: false},
+      {id: 1, text: 'Take out the trash', isComplete: false},
+      {id: 2, text: 'Eat diiner', isComplete: false},
+      {id: 3, text: 'Play with the puppy dogs', isComplete: false},
+      {id: 4, text: 'Go to bed before midnight', isComplete: false},
+  ]
+  })
   render() {
   return (
-    <div className="App">
+    <div className="TodoApp">
+      <h1>Stuff I've been putting off</h1>
+      {/* <TodoForm /> */}
+      {/* <TodoList /> */}
     </div>
   );
 }
